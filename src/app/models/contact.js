1e08e572f1b8b08ca8082abae349136c27630c42
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2016-11-29 14:31:27 -0800

    added contact model subclass

--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,6 +1,18 @@
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
+  defaults: {
+    name: "Placeholder Name",
+    phoneNumber: "2061110000",
+    email: "Placeholder Email", // This is unnecessary since undefined is falsey.
+  },
+
+  initialize: function (options) {
+    console.log("Task Contact for: " +
+    this.get("name"));
+    // this.set("description", "JavaScript is frustrating!")
+  },
+
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
 });
