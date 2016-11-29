f36d55985c244168def47cbb79c58f329f270720
laurenfb <laurenfriesbrink@gmail.com>
2016-11-29 15:39:04 -0800

    tryna make a contact view?

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,26 @@
+import $ from 'jquery';
 import Backbone from 'backbone';
+import _ from 'underscore';
+
 
 const ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    this.name = options.name;
+    this.email = options.email;
+    this.phone = options.phone;
+    // this.listenTo(this.model, "change", this.render);
+  },
+
+  render: function() {
+    // var html = this.template({task: this.model.toJSON()})
+    // this.$el.html(html);
+    // // reconnects the DOM event handlers
+    // this.delegateEvents();
+    // // Enable chained calls
+    return this;
+  }
+
 });
 
+// accessible elsewhere
 export default ContactView;
