2d6940dfe560aef76a40cb03cb9467982825ccea
Qlwentt <qlwentt@gmail.com>
2016-11-29 15:23:02 -0800

    Have a single instance of Contact created from static data.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from 'app/models/contact'
+import $ from 'jquery';
+
 
 var application = new Application();
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+// var appView = new ApplicationView({
+//   el: '#application',
+//   model: application
+// });
+
+var contactData=  
+{
+    name: "Quai",
+	email: "whatever@gmail.com",
+	phone: "8455551234"
+};
+
+$(document).ready(function() {
+  var contact = new Contact(contactData);
+  var application = new ApplicationView({
+    el: $('#application'),
+    model: contact
+  });
+  application.render();
 });
