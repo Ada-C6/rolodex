567746467806466766f2e809a709ef48cebdc019
laurenfb <laurenfriesbrink@gmail.com>
2016-11-30 08:56:42 -0800

    omg i made wave 1 work, thanks to the brilliant and lovely TA Bri

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from 'app/models/contact';
+import ContactView from 'app/views/contact_view';
+import $ from 'jquery';
 
 var hardCodedContact = {
   name: "christian",
@@ -7,7 +10,16 @@ var hardCodedContact = {
   phone: "6126550000"
 };
 
-var contact = new Contact(hardCodedContact)
+$(document).ready(function(){
+
+  var contact = new Contact(hardCodedContact);
+
+  var contactView = new ContactView({
+    el: '#contact-cards',
+    model: contact 
+  });
+})
+
 
 // var application = new Application();
 //
