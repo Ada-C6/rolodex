ad993197b20878a27674882af206269f8b4abde0
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-30 14:33:24 -0800

    added default info and initialize

--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,16 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+
+  defaults: {
+  	name: "Sarsparilla Rootbier",
+  	phone: "000-000-0000",
+  	email: "srb@beveragecemetary.io"
+  },
+
+  initialize: function() {
+  	console.log("created new contact card for" + this.name);
+  }
 });
 
 export default Contact;
