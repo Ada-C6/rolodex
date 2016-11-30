ba80486151ed7daf64d0cf29d2d6c1f97dcbbd32
Yeni <yenicapote08@gmail.com>
2016-11-30 14:53:20 -0800

    added intialize with template and listen events, and render.

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,25 @@
 import Backbone from 'backbone';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    // this.task = options.task;
+    //have to tell backbone how to handle it
+    this.template = options.template;
+
+    this.listenTo(this.model, 'change', this.render);
+
+  },
+
+  render: function() {
+    var html = this.template({contact: this.model.attributes});
+      //will create a freestanding div to put into the page to be part of the DOM once it is appended
+    this.$el.html(html);
+
+    //re-attach DOM event listeners to our brand-spankin-new HTML
+    this.delegateEvents();
+    // Enable chained calls
+    return this;
+  }
 });
 
 export default ContactView;
