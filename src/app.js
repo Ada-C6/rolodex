c4bba29e54ab49ec32c849edfc1e65e7dcacbfd6
Brandi Phillips <milligan.brandi@gmail.com>
2016-11-30 15:21:30 -0800

    I gotta name\!

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
+import $ from 'jquery';
+import Backbone from 'backbone';
+import _ from 'underscore';
+import ContactView from 'app/views/contact_view';
+
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
-var application = new Application();
+var contactData = [
+  {
+    name: "Malika",
+    email: "Malika.NZY@gmail.com",
+    phone: "206-555-1212"
+  }
+];
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+
+$(document).ready(function(){
+  var contactTemplate = _.template($('#tmpl-contact-card').html());
+  var card = new ContactView({name: contactData[0].name, template: contactTemplate});
+  console.log(card.name);
+  $('#contact-cards').append(card.render().$el);
+
+  var application = new Application();
+  var appView = new ApplicationView({
+    el: '#application',
+    model: application
+  });
 });
