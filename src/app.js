b9e16ab57e524d8c9b7905a09de0dc1727c07b56
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-11-30 11:52:55 -0800

    Displays one contact card without form

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ import $ from 'jquery';
 
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from 'app/models/contact';
+import ContactView from 'app/views/contact_view';
+
+var contactData = {
+    name: 'Freddie',
+    phoneNumber: 123456,
+    email: 'fred@msn.com'
+  };
 
 $(document).ready(function() {
-  var application = new Application();
-
-  var appView = new ApplicationView({
+  var contact = new Contact(contactData);
+  console.log(contact.attributes.name);
+  var contactView = new ContactView({
     el: '#application',
-    model: application
+    model: contact
   });
 
-  appView.render();
+  // var application = new Application();
+
+  // var appView = new ApplicationView({
+  //   el: '#application',
+  //   model: application
+  // });
+
+  contactView.render();
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,29 @@
+import $ from 'jquery';
+import _ from 'underscore';
 import Backbone from 'backbone';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    // console.log('options: ' + options.model.attributes.name);
+    this.template = _.template($('#tmpl-contact-card').html());
+    this.name = options.model.attributes.name;
+  },
+
+  render: function() {
+    console.log(this.model.attributes);
+    console.log(this.template);
+    var html = this.template({name: this.name});
+    this.$el.html(html);
+    console.log(html);
+
+    // // Re-attach DOM event listeners to our new HTML
+    // this.delegateEvents`();
+
+    // Enable chained calls
+    return this;
+  }
+
+
 });
 
 export default ContactView;
