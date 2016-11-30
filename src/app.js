12f4cfcb808c8ee5cde19ceb377cb2fcb5801179
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-30 09:40:34 -0800

    Wave One completed. Hiding the details section is a little bit wonky. THis should clear up with wave 3 I presume.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from 'app/models/contact';
+import ContactView from 'app/views/contact_view';
+import $ from 'jquery';
 
-var application = new Application();
+var contactData = [
+  {
+    name: 'Kapi-san',
+    email: 'kapi@bara.com',
+    phone: 2
+  }
+
+];
+
+
+var application = new Application(contactData);
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+$(document).ready(function(){
+  var cont = new Contact(contactData[0]);
+  var contView = new ContactView({
+    el: $('#contact-cards'),
+    model: cont
+  });
+  contView.render();
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,20 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    this.contact = options.model;
+    this.template = _.template($('#tmpl-contact-card').html());
+  },
+  render: function() {
+    console.log(this.model.attributes);
+    console.log("This should be the name: " + this.model.get('name'));
+    var html = this.template({name: this.model.get('name')});
+    this.$el.html(html);
+    $('#contact-details').hide();
+    return this;
+  }
 });
 
 export default ContactView;
