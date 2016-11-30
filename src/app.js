ecd6d4a7a7c7e4395570639d919f241894b14fcb
Jessica <jchung722@gmail.com>
2016-11-30 11:05:05 -0800

    omg wave 1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import Backbone from 'backbone';
 import Application from 'app/models/application';
+import Contact from 'app/models/contact';
 import ApplicationView from 'app/views/application_view';
+import ContactView from 'app/views/contact_view';
 
-var application = new Application();
+var contactData = [
+  {
+    name: 'Jane',
+    phone: '123-4567',
+    email: 'Janedoe2@gmail.com'
+  }
+];
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+$(document).ready(function() {
+  var contact = new Contact(contactData[0]);
+  var contactTemplate = _.template($('#tmpl-contact-details').html());
+  // var application = new Application(contactData);
+
+  var contactView = new ContactView({
+    el: $('#contact-details'),
+    model: contact,
+    template: contactTemplate
+  });
+
+  contactView.render();
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,22 @@
+import $ from 'jquery';
+import _ from 'underscore';
 import Backbone from 'backbone';
+import Contact from 'app/models/contact';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    this.template = options.template;
+  },
+
+  render: function() {
+    console.log(this.template);
+    var html = this.template({contact: this.model.attributes});
+    this.$el.html(html);
+    console.log(this.model);
+
+    // Enable chained calls
+    return this;
+  }
 });
 
 export default ContactView;
