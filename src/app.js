5b5117f2f9f7d40f46e214fa2e310f5ff1354bd2
Alma Maizels <aymaiz@yahoo.com>
2016-11-30 09:11:46 -0800

    work on wave 1 - nothing works just yet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
+import $ from 'jquery';
+
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
-var application = new Application();
+var contactData = [
+  {name: 'Erik', email: 'eam@dream.of', phone: '008-008 8888'},
+  {name: 'Fia', email: 'sam@dream.of', phone: '008-008 3333'},
+  {name: 'Ben', email: 'BM@dream.of', phone: '008-008 2222'}
+];
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+var application = new Application(); // from cloned doc
+
+$(document).ready(function() {
+  var listElement = $('#contact-cards');
+
+  var contactOptions = {
+    el: '#application',
+    model: application
+  };
+
+  var appView = new ApplicationView(contactVariableOfSomeSort);
+
+  var contactView = new ContactView({
+    el: '#contact-cards',
+    model: contact
+  });
+
+  appView.render();
+  contactView.render();
+
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,38 @@
 import Backbone from 'backbone';
 
-const ContactView = Backbone.View.extend({
+// const ContactView = Backbone.View.extend({
+// });
+// ALL COPIED FROM TASKS
+var ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    this.template = _.template($('tmpl-contact-card').html());
+
+    this.listenTo(this.model, 'change', this.render);
+  },
+
+  render: function() {
+    var html = this.template({contact: this.model.attributes});
+    this.$el.html(html);
+
+    // Re-attach DOM event listeners to our new HTML
+    this.delegateEvents();
+    // Enable chained calls
+    return this;
+  },
+
+  events: {
+    'click .delete-button': 'deleteHandler'
+  },
+
+  deleteHandler: function() {
+    // Show a popup box asking the user for confirmation
+    if (window.confirm("Are you sure you want to delete this Contact?")) {
+      this.model.destroy(); // implicitly calls 'remove'
+      // Remove calls: 'remove' on the collection and 'update' on the collection
+      console.log('This contact is destroyed');
+    }
+  }
+
 });
 
 export default ContactView;
