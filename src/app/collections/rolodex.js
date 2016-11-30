30daadde68745318d9453c12908da03b7e324dc2
jm-rives <joanna.rives@gmail.com>
2016-11-30 09:05:49 -0800

    REVISE location of document ready

--- src/app/collections/rolodex.js
+++ src/app/collections/rolodex.js
@@ -1,4 +1,4 @@
-// rolodex holds the contact cards plus its own stuff
+
 import Backbone from 'backbone';
 
 const Rolodex = Backbone.Collection.extend({
@@ -8,11 +8,4 @@ const Rolodex = Backbone.Collection.extend({
   // Contact.
 });
 
-$(document).ready(function){
-  var rolodex = new Rolodex();
-  var application = new ApplicationView({
-    el: $('application')
-    // model goes here, later, I think
-  });
-}
 export default Rolodex;
--- src/app/models/application.js
+++ src/app/models/application.js
@@ -1,9 +1,20 @@
-import Backbone from 'backbone';
 import $ from 'jquery';
+import Backbone from 'backbone';
 import _ from 'underscore';
 
 const Application = Backbone.Model.extend({
   // This model represents the overall application.
 });
+//  modify for contacts
+$(document).ready(function() {
+  // var taskList = new TaskList(taskData);
 
+  // var viewContacts = new ContactView({
+  //   el: $('#application'),
+  //   model: Contact
+  // });
+
+  // application.render();
+
+});
 export default Application;
