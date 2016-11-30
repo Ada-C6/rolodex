1c7921182923fb41f890c7e39db01ecdfb56a0db
RedSquirrelious <redsquirrelious@gmail.com>
2016-11-30 14:32:51 -0800

    trying to render

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
-var application = new Application();
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+$(document).ready(function() {
+	var application = new Application();
+
+	var appView = new ApplicationView({
+	  el: '#application',
+	  model: application
+	});
+  application.render();
 });
