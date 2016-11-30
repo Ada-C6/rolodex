43a0d3a9b7e3f5808804350168e34d117f257a55
Miriam Cortes <mcortes.umiami@gmail.com>
2016-11-29 17:14:47 -0800

    contacts data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
+var contactsData = [
+  {
+    name: "Pancho Perez",
+    email: "ju kidin me? we no got no fones!",
+    phone: "dos latas y un cordel"
+  },
+  {
+    name: "Juanita Sanchez",
+    email: "pero y este que se piensa?",
+    phone: "guan-tu...que dice?"
+  },
+  {
+    name: "Perequita Pita",
+    email: "hay que encenderle una velita a San Lazaro",
+    phone: "oigo???"
+  }
+];
+
 var application = new Application();
 
 var appView = new ApplicationView({
