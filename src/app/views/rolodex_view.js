d87925c7376ba9080c852ba693ff3879d7f08cba
Miriam Cortes <mcortes.umiami@gmail.com>
2016-11-30 10:58:38 -0800

    wave 2: save and cancel buttons working

--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -9,6 +9,7 @@ const RolodexView = Backbone.View.extend({
     this.contactsData = options.contactsData; //stores the full list of contacts
     this.contactTemplate = _.template($('#tmpl-contact-card').html()); //compiles a template to be shared between the contacts
     this.listElement = this.$('#contact-cards'); //keep track of the <li> elements
+
     this.cardList = [];
     this.contactsData.forEach(function(contact) {
       var card = new ContactView({
@@ -17,6 +18,12 @@ const RolodexView = Backbone.View.extend({
       });
       this.cardList.push(card);
     }, this);
+
+    this.input = {
+      name: this.$('.contact-form input[name="name"]'),
+      email: this.$('.contact-form input[name="email"]'),
+      phone: this.$('.contact-form input[name="phone"]')
+    };
   }, //close initialize
 
   render: function() {
@@ -26,7 +33,43 @@ const RolodexView = Backbone.View.extend({
       this.listElement.append(card.$el); //add the html to the rolodex
     }, this);
     return this;
-  } //close render
+  }, //close render
+
+  events: {
+    'click .btn-save': 'createContact',
+    'click .btn-cancel': 'clearInput'
+  }, //close events
+
+  clearInput: function(event) {
+    event.preventDefault();
+    console.log('clear input button pressed');
+    this.input.name.val('');
+    this.input.email.val('');
+    this.input.phone.val('');
+  }, //close clearInput
+
+  createContact: function(event) {
+    event.preventDefault();
+    console.log('creating a contact button pressed!');
+    var contact = this.getInput();
+    this.contactsData.push(contact);
+    var card = new ContactView({
+      contact: contact,
+      template: this.contactTemplate
+    });
+    this.cardList.push(card);
+    this.render();
+    this.clearInput(); //this is causing an error in my console log but it's still working...:shrugs:
+  }, //close createContact
+
+  getInput: function() {
+    var contact = {
+      name: this.input.name.val(),
+      email: this.input.email.val(),
+      phone: this.input.phone.val()
+    };
+    return contact;
+  }//close getInput
 });
 
 export default RolodexView;
