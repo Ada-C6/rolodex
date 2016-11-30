d0e0704f6c63378192dcc5d320ceee99bd8523cd
jm-rives <joanna.rives@gmail.com>
2016-11-29 16:22:47 -0800

    ADD document ready fxn

--- src/app/collections/rolodex.js
+++ src/app/collections/rolodex.js
@@ -8,4 +8,11 @@ const Rolodex = Backbone.Collection.extend({
   // Contact.
 });
 
+$(document).ready(function){
+  var rolodex = new Rolodex();
+  var application = new ApplicationView({
+    el: $('application')
+    // model goes here, later, I think
+  });
+}
 export default Rolodex;
