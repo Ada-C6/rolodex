ccd04c968715e02cf926b2a18957e365d1dd745a
Alyssa Hursh <akhursh@gmail.com>
2016-11-30 12:13:01 -0800

    Completed second bullet of Wave 2

--- src/app.js
+++ src/app.js
@@ -1,34 +1,80 @@
+import $ from 'jquery';
+import _ from 'underscore';
+
+// Import the Application model and view:
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+
+// Import the contact model and view:
 import Contact from 'app/models/contact';
 import ContactView from 'app/views/contact_view';
 
+// Import the collections:
+import Rolodex from 'app/collections/rolodex';
+import RolodexView from 'app/views/rolodex_view';
 
+// Create a few first contact from the Contact model
 var firstContact = new Contact ({
   name: "Olivia",
-  phone: "123456789",
+  phone: "1234567890",
   email: "justin@bieber.com"
 });
 
-console.log("WHEN THIS NEXT LINE ISN'T UNDEFINED YOU WIN")
-console.log(firstContact.attributes.name);
+// Create a list of new contacts to pass around
+var contactList = [
+  {
+    name: "Leah",
+    phone: "234567890",
+    email: "leah@leah.com"
+  },
+  {
+    name: "Alyssa",
+    phone: "34567890",
+    email: "alyssa@alyssa.com"
+  },
+  {
+    name: "Sabrina",
+    phone: "4567890",
+    email: "sabrina@sabrina.com"
+  },
+  {
+    name: "Damien",
+    phone: "567890",
+    email: "damien@damien.com"
+  },
+];
 
-console.log("You're in the app.js file and you're between making a contact and making a contactView'");
 
-var oneContact = new ContactView ({
-  contact: firstContact,
+// This should fire after the document is ready:
+$(document).ready(function(){
+  console.log("The document is ready");
+  var someContacts = new Rolodex(contactList);
+  var contactCardTemplate = _.template($('#tmpl-contact-card').html());
+
+
+  console.log("The Rolodex has been created");
+  console.log(someContacts);
+
+  var roloView = new RolodexView({
+    el: $('#application'),
+    contacts: someContacts,
+    template: contactCardTemplate
+  });
+
+  console.log("The application is about to be rendered");
+  roloView.render();
 });
 
-console.log("You just made a contactView and now you're trying to render it");
-
-oneContact.render();
-
-console.log("Technically, this rendered???");
 
 
-var application = new Application();
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
-});
+
+
+
+// This was provided to me when I opened the original files:
+// var application = new Application();
+//
+// var appView = new ApplicationView({
+//   el: '#application',
+//   model: application
+// });
--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -1,6 +1,33 @@
 import Backbone from 'backbone';
+import $ from 'jquery';
 
 const RolodexView = Backbone.View.extend({
+  initialize: function(incomingContacts) {
+    console.log("You've created the RolodexView");
+    console.log(incomingContacts);
+
+    this.contacts = incomingContacts.contacts;
+    this.template = incomingContacts.template;
+  },
+  render: function() {
+    console.log("You are rendering the RolodexView");
+    console.log(this);
+
+    this.placement = $("#contact-cards");
+
+    this.contacts.forEach(function(individualContact){
+      console.log("You're in the RolodexView initialize one contact!");
+      console.log(individualContact.attributes.name);
+      console.log(this.template);
+
+
+      var html = this.template({name: individualContact.attributes.name, email: individualContact.attributes.email, phone: individualContact.attributes.phone});
+
+      this.placement.append(html);
+    }, this);
+
+    return this;
+  }
 });
 
 export default RolodexView;
