fc9248b9fc66e1cbc41709323eea373190f692a9
Karin Kubischta <karin.kubischta@gmail.com>
2016-11-29 18:37:45 -0800

    got contact_view and template working

--- src/app.js
+++ src/app.js
@@ -1,9 +1,53 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import Backbone from 'backbone';
+
+import ContactView from 'app/views/contact_view';
+import Contact from 'app/models/contact';
+
+
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+
+var peeps = [
+  {
+    name: "karin",
+    email: "k@ada.edu",
+    phone: "206-222-2121"
+  },
+  {
+    name: "sky",
+    email: "sky@ada.edu",
+    phone: "206-222-2121"
+  },
+  {
+    name: "esther",
+    email: "esther@ada.edu",
+    phone: "206-222-2121"
+  }
+];
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+$(document).ready(function(){
+  var contactCardTemplate = _.template($('#tmpl-contact-card').html());
+
+  var contactListElement = $('#contact-cards');
+
+  var cardList = [];
+
+  peeps.forEach(function(contactDeets) {
+    var card = new ContactView({
+      contact: contactDeets,
+      template: contactCardTemplate
+    });
+    cardList.push(card);
+    contactListElement.append(card.render().$el);
+    card.render();
+  });
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,20 @@
 import Backbone from 'backbone';
+import Contact from 'app/models/contact';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(options) {
+    this.contact = options.contact;
+    this.template = options.template;
+    console.log(this.contact);
+  },
+
+  render: function() {
+    var html = this.template({name: this.contact.name});
+
+    this.$el.html(html);
+    // Enable chained calls
+    return this;
+  },
 });
 
 export default ContactView;
