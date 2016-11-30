2b7a1d1bde2669fe21d1ac50ab4574119780f5ab
Yeni <yenicapote08@gmail.com>
2016-11-29 16:28:48 -0800

    Added default model attributes.

--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,14 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  defaults: {
+    name: "cat",
+    phoneNumber: 2063456789,
+    email: "meow@purr.com"
+  },
+  initialize.function() {
+    console.log("Creating a new contact with name " + this.name);
+  }
 });
 
 export default Contact;
