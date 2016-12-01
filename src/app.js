e2ea3e2b318af6778ab4d815786ab591b432fdfd
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-01 11:25:10 -0800

    created static data for one contact in the app.js file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
-var application = new Application();
+var contactData = [
+    {
+        name: 'hello world',
+        email: 'testing@world.com',
+        phone: '2345678901'
+    }
+];
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+$(document).ready(function() {
+    var contactList = new ContactList(contactData);
+    var options = {
+        el: '#application',
+        model: application
+    };
+    var application = new ContactListView(options);
+    application.render();
 });
+
+// var application = new Application();
+//
+// var appView = new ApplicationView({
+//     el: '#application',
+//     model: application
+// });
