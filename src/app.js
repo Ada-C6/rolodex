1c5df396b05c9fec2c1f03728fd9148de570a98b
Carmen Chan <carmenkellychan@gmail.com>
2016-12-01 13:04:51 -0800

    completed wave 1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import _ from 'underscore';
+import $ from 'jquery';
+
+import Contact from 'app/models/contact';
+import ContactView from 'app/views/contact_view';
 
 var application = new Application();
 
@@ -7,3 +12,29 @@ var appView = new ApplicationView({
   el: '#application',
   model: application
 });
+
+var contactData = [
+  {
+    name: 'Charles',
+    email: 'charles@email.com',
+    phone: '0987654321'
+  },
+  {
+    name: 'Chris',
+    email: 'chris@email.com',
+    phone: '3216540987'
+  }
+];
+
+var charlesmodel = new Contact(contactData[0]);
+
+var contactTemplate = _.template($('#tmpl-contact-card').html());
+// console.log("contact Template: ", contactTemplate);
+
+var charlesview = new ContactView({
+  model: charlesmodel,
+  template: contactTemplate
+});
+// console.log("charlesview: ", charlesview);
+
+$('#contact-cards').append(charlesview.render().$el);
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,8 +1,20 @@
+// contact.js
+
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+
+  defaults: {
+    name: "Person",
+    phone: "1234567890",
+    email: "email@email.com"
+  },
+
+  initialize: function() {
+    // console.log("contact created: " + this.name);
+  }
 });
 
 export default Contact;
