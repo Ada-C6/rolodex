c972b3e6a9820dd821335170b877dadc52046b64
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-01 11:37:17 -0800

    implemented foundational structure for ContactView

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,20 @@
 import Backbone from 'backbone';
 
 const ContactView = Backbone.View.extend({
+    initialize: function(options) {
+        this.template = options.template;
+    },
+
+    render: function() {
+        console.log(this.model.attributes);
+        var html = this.template({contact: this.model.attributes});
+        this.$el.html(html);
+
+        // will need to uncomment this once I re-render views:
+        this.delegateEvents(); // this re-attaches DOM event listeners to our 'brand new' HTML every time we re-render the view
+
+        return this;
+    }
 });
 
 export default ContactView;
