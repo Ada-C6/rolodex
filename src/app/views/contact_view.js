d0f930a3a7442e78fcb89c63194c91e0d986c7c6
Allison Hoke <allisonhoke@gmail.com>
2016-12-01 16:53:28 -0800

    WIP

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -9,6 +9,10 @@ import Contact from 'app/models/contact';
 const ContactView = Backbone.View.extend({
   initialize: function(options) {
     this.template = _.template($('#tmpl-contact-card').html());
+
+    this.modalTemplate = _.template($('#tmpl-contact-details').html());
+
+    this.listenTo(this, "showContactDetails", this.viewDetails);
   },
 
   render: function() {
@@ -16,6 +20,25 @@ const ContactView = Backbone.View.extend({
     // console.log("===========" + this.model.attributes);
     this.$el.html(html);
     return this;
+  },
+
+  events: {
+    'click .contact-card': 'showDetails'
+  },
+
+  // toggleDetails: function(onIndicator) {
+  //   $('#tmpl-contact-details').toggle(!onIndicator);
+  // },
+
+  showDetails: function() {
+    console.log("SHOW SOME DETAILS HERE");
+    //trigger something
+    this.trigger("showContactDetails");
+  },
+
+  viewDetails: function() {
+    //show the details card somehow - ask google
+    console.log("VIEWING THE DETAILS CARD");
   }
 });
 
