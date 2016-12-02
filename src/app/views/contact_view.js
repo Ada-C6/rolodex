d651f05fc5d818895569a27e8ac5ce2a6a939cd7
Yeni <yenicapote08@gmail.com>
2016-12-02 15:53:38 -0800

    Added the showContact function to show the contact details and an event handler.

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,13 +1,20 @@
 import Backbone from 'backbone';
-
+import _ from 'underscore';
+import $ from 'jquery';
 const ContactView = Backbone.View.extend({
   initialize: function(options) {
     // this.task = options.task;
+    console.log(options);
     //have to tell backbone how to handle it
     this.template = options.template;
+    this.contactTemplate = _.template($('#tmpl-contact-details').html());
 
     this.listenTo(this.model, 'change', this.render);
 
+  },
+
+  events: {
+    'click .contact-card': 'showContact'
   },
 
   render: function() {
@@ -19,6 +26,15 @@ const ContactView = Backbone.View.extend({
     this.delegateEvents();
     // Enable chained calls
     return this;
+  },
+
+  showContact: function() {
+    event.stopPropagation();
+
+    $('#contact-details').show();
+
+    var contactDetails = this.contactTemplate({contact:this.model.attributes});
+    $('#contact-details').html(contactDetails);
   }
 });
 
