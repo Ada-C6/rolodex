77c78f84f0d1bbae40611b6e940f6f93f61f2a7a
Yeni <yenicapote08@gmail.com>
2016-12-02 15:55:30 -0800

    changed element from #application to html to have it be the entire html file.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
-import Application from 'app/models/application';
-import ApplicationView from 'app/views/application_view';
-import Contact from 'app/models/contact';
-import ContactView from 'app/views/contact_view';
+
+
 import Rolodex from 'app/collections/rolodex';
 import RolodexView from 'app/views/rolodex_view';
 
@@ -25,17 +23,18 @@ var contactData = [
 ];
 
 
+// var application = new Application();
+//
+// var appView = new ApplicationView({
+//   el: '#application',
+//   model: application
+// });
 $(document).ready(function() {
-  // var application = new Application();
-  //
-  // var appView = new ApplicationView({
-  //   el: '#application',
-  //   model: application
-  // });
 
   var contact = new Rolodex(contactData);
+
   var options = {
-    el: $('#application'),
+    el: 'html',
     model: contact
   };
   var rolodex = new RolodexView(options);
