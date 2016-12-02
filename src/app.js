a79df2cdfc3475ad49d4dd6c1bb362f8ad64dff7
RedSquirrelious <redsquirrelious@gmail.com>
2016-12-01 16:48:08 -0800

    working card

--- src/app.js
+++ src/app.js
@@ -1,13 +1,40 @@
-import Application from 'app/models/application';
-import ApplicationView from 'app/views/application_view';
+// import Application from 'app/models/contact';
+// import ApplicationView from 'app/views/contact_view';
+import $ from 'jquery';
+import _ from 'underscore';
+import Backbone from 'backbone';
+import Contact from 'app/models/contact';
+import ContactView from 'app/views/contact_view';
+
+
+// $(document).ready(function() {
+// 	var application = new Application();
+
+
+// 	var appView = new ApplicationView({
+// 	  el: '#application',
+// 	  model: application
+// 	});
+//   application.render();
+// });
+
 
 
 $(document).ready(function() {
-	var application = new Application();
 
-	var appView = new ApplicationView({
-	  el: '#application',
-	  model: application
+
+
+	var contact = new Contact();
+
+	console.log(contact.attributes.email);
+
+	var contactView = new ContactView({
+	  el: $('#contact-cards'),
+	  model: contact
 	});
-  application.render();
+	// console.log(contactView);
+
+  contactView.render();
 });
+
+// make a list, stop using contactView here and go with a forEach in the rolodex file
