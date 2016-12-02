c434372aa13ebfeb609b37db427f73a8e6e667da
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-01 16:00:00 -0800

    deleted commented lines no longer necessary

--- src/app.js
+++ src/app.js
@@ -3,18 +3,6 @@ import ApplicationView from 'app/views/application_view';
 
 import $ from 'jquery';
 
-// import RolodexView from 'app/views/rolodex_view';
-//
-// import Rolodex from 'app/collections/rolodex';
-
-// var contactData = [
-//     {
-//         name: 'hello world',
-//         email: 'testing@world.com',
-//         phone: '2345678901'
-//     }
-// ];
-
 // $(document).ready(function() {
 //     var contactList = new Rolodex(contactData);
 //     var options = {
