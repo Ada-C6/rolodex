0b171b10e25f43db917c8eb124f3dc911d185ca9
laurenfb <laurenfriesbrink@gmail.com>
2016-12-01 20:59:17 -0800

    OMG I MADE A CLEAR BUTTON THAT WORKS? YAY

--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -7,8 +7,28 @@ const ApplicationView = Backbone.View.extend({
   },
 
   render: function() {
+    // console.log("WE ARE RENDERING AN APPLICATION VIEW!")
     return this;
-  }
+  },
+
+  events: {
+    'click .btn-cancel': 'clearInput',
+    'click .btn-save': 'saveButton'
+  }, //end of events
+
+  clearInput: function(event) {
+    event.preventDefault();
+    console.log("clearing");
+    $('input[name="name"]').val("");
+    $('input[name="email"]').val("");
+    $('input[name="phone"]').val("");
+
+  }, // end of clear
+
+  saveButton: function(event) {
+    console.log("saving that")
+  } // end of save
+
 });
 
 export default ApplicationView;
