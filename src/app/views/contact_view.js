264486a1d8c139789eec20c9b33a3df728b9368f
RedSquirrelious <redsquirrelious@gmail.com>
2016-12-01 16:49:25 -0800

    working card w template

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,8 +1,43 @@
+import $ from 'jquery';
+import _ from 'underscore';
 import Backbone from 'backbone';
-
 import Contact from 'app/models/contact';
 
 const ContactView = Backbone.View.extend({
+	initialize: function(options) {
+
+//GOING WITH EASY FOR NOW, MVP
+
+		this.template = _.template($('#tmpl-contact-card').html());
+
+		this.model = options.model
+
+		this.listElement = this.$("contact-cards");
+
+  }, //initialize end
+
+  render: function() {
+
+  	var html = this.template({name: this.model.attributes.name});
+  // var html = this.model.attributes.name;
+
+    console.log("red " + html);
+
+  this.$el.html(html);
+
+
+    // Enable chained calls
+    return this;
+  }, //render end
+
+  addContact: function(contact) {
+  	var card = new Contact({
+  	model: contact,
+  	template: this.template
+  })
+  },
+
+
 });
 
 export default ContactView;
