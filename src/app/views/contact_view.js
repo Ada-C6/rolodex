b74f9d203c8251f0e466ec046df2d21e7fc97f33
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-12-02 10:37:19 -0800

    added click event and created a showHandler that triggers a 'show' event and passes the model

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -13,6 +13,15 @@ const ContactView = Backbone.View.extend({
         this.delegateEvents(); // this re-attaches DOM event listeners to our 'brand new' HTML every time we re-render the view
 
         return this;
+    },
+
+    events: {
+        'click': 'showHandler'
+    },
+
+    showHandler: function(event) {
+        console.log('showHandler called');
+        this.trigger('show', this.model);
     }
 });
 
