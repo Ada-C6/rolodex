4711ffe585471c9a373f562a6c0c62f125841795
RedSquirrelious <redsquirrelious@gmail.com>
2016-12-02 16:56:30 -0800

    add modalView to initialize, add zoomHandler, add listener

--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -4,23 +4,30 @@ import Backbone from 'backbone';
 
 import ContactView from 'app/views/contact_view';
 import Contact from 'app/models/contact';
+import ModalView from 'app/views/modal_view';
 
 
 const RolodexView = Backbone.View.extend({
 	initialize: function(options) {
 		//make a template
 		this.contactTemplate = _.template($('#tmpl-contact-card').html());
+
 // track my list element 
 		this.listElement = this.$('#contact-cards');
 
 // track my contact card views
 		this.cardList = [];
 
-
+		this.modalView = new ModalView({
+			el: this.$('#contact-details'),
+			model: null
+		});
 
 // process my contact cards
 		this.model.forEach(function(contact) {
 			this.addContact(contact);
+			// this.listenTo(contact, 'cardPicked');
+
 		}, this );
 
 // take in new contacts
@@ -33,14 +40,14 @@ const RolodexView = Backbone.View.extend({
 // re-render the list when my rolodex/contact collection changes
 		this.listenTo(this.model, 'update', this.render );
 // when a model is removed from the collection, take their contact from the rolodex
-		// this.listenTo(this.model, 'remove', this.removeContact );
+
 	}, //initialize end
 
 
 
 	events: { 
 		'click .btn-save': 'createContact',
-		'click .btn-cancel': 'clearInput'
+		'click .btn-cancel': 'clearInput',
 	},  //events end
 
 
@@ -52,6 +59,7 @@ const RolodexView = Backbone.View.extend({
 // // loop through the array of contact cards
 		this.cardList.forEach(function(card) {
 			//show the card
+
 			card.render();
 
 			this.listElement.append(card.$el);
@@ -68,8 +76,12 @@ const RolodexView = Backbone.View.extend({
   addContact: function(contact) {
   	var card = new ContactView({
   	model: contact,
-  	template: this.contactTemplate
+  	template: this.contactTemplate,
   });
+  	this.listenTo(card, 'cardPicked', this.zoomHandler);;
+
+  	// console.log(card);
+
   	this.cardList.push(card);
   }, //addContact end
 
@@ -111,6 +123,10 @@ const RolodexView = Backbone.View.extend({
   	this.clearInput();
   }, //createContact end
 
+  zoomHandler: function(card) {
+  	this.modalView.model = card.model;
+  	this.modalView.render().$el.show();
+  }
 
 });  //RolodexView end
 
