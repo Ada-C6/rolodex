91fed1806c1d588790411bc2ac47551f313d38fd
Brandi Phillips <milligan.brandi@gmail.com>
2016-12-11 15:23:23 -0800

    trying to get wave two to work without success

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import ContactView from 'app/views/contact_view';
 import Rolodex from 'app/collections/rolodex';
+import RolodexView from 'app/views/rolodex_view';
 
 
 import Application from 'app/models/application';
@@ -13,6 +14,10 @@ var contactData = [
     name: "Malika",
     email: "Malika.NZY@gmail.com",
     phone: "206-555-1212"
+  }, {
+    name: "Taybor",
+    email: "Tay@ada.com",
+    phone: "206-123-1234"
   }
 ];
 
@@ -21,13 +26,17 @@ $(document).ready(function(){
   var contactTemplate = _.template($('#tmpl-contact-details').html());
 
   var cardList = new Rolodex(contactData);
-
+  var rolodex = new RolodexView({
+    el: $('#application'),
+    model: cardList
+  });
+  rolodex.render();
   // $('#contact-details').append(cardList[0].render().$el);
 
   var application = new Application();
   var appView = new ApplicationView({
-    el: $('#application'),
-    model: cardList
+    el: '#application',
+    model: application
   });
-  application.render();
+  // application.render();
 });
--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -8,26 +8,32 @@ import ContactView from 'app/views/contact_view';
 
 const RolodexView = Backbone.View.extend({
   initialize: function(options) {
-    this.modelList = [];
+
     // template for the individual contact cards
     this.contactCardTemplate = _.template($('#tmpl-contact-details').html());
+
     // keep track of the <ul> element
     this.listElement = this.$('#contact-cards');
-    // create a ContactView for each contactCard:
-    // list of contact card model instances:
+
+    // list of card views:
     this.cardList = [];
+
     // process each task as they are created:
     this.model.forEach(function(contact){
       this.addContact(contact);
     }, this); // bind 'this' so it's available inside forEach (????)
     this.input = {
-      name: this.$(''),
-      email: this.$('')
-      phone: this.$('')
+      name: this.$('.contact-form input[name="name"]'),
+      email: this.$('.contact-form input[email="email"]'),
+      phone: this.$('.contact-form input[phone="phone"]')
     };
 
-    // listen for the save click
+    // when a model is added to the collection, add a contactCard for it
     this.listenTo(this.model, 'add', this.addContact);
+
+    // re-render the list when the collection changes:
+    this.listenTo(this.model, 'update', this.render);
+
   },
   render: function() {
     // need to make sure the list in the DOM is empty before appending new items
@@ -40,10 +46,21 @@ const RolodexView = Backbone.View.extend({
     }, this);
     return this;
   },
+  events: {
+    'submit .contact-form': 'createContact',
+    'click .btn-cancel': 'clearInput'
+  },
+
   createContact: function(event) {
     event.preventDefault(); // supress the refreshing of page with a form submission
-    var contact = new Contact(this.getInput());
-    this.model.add(contact);
+
+    // get the input and turn it into a contact:
+    var rawContact = this.getInput();
+
+    var contact = this.model.add(rawContact);
+
+    this.addTask(task);
+    this.render();
     // clear the input form:
     this.clearInput();
   }, // end create contact
@@ -62,6 +79,14 @@ const RolodexView = Backbone.View.extend({
       model: contact,
       template: this.contactTemplate
     });
+    // add the card to the list
+    this.cardList.push(contactCard);
+  },
+
+  clearInput: function(event){
+    this.input.name.val('');
+    this.input.email.val('');
+    this.input.phone.val('');
   }
 
 }); // end of RolodexView
