6a4ba5a6faec3bf9ed38b1b076f546ba495cc951
Melissa Rodriguez <olenellina@gmail.com>
2017-01-06 11:08:11 -0800

    wave 1 complete

--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,8 +1,23 @@
+// When wave 1 is complete your application should:
+//
+// Have a Backbone Model subclass called Contact. This model should have these attributes:
+// Name
+// E-mail address
+// Phone number
+// Have a single instance of Contact created from static data.
+// Have a Backbone View subclass called ContactView.
+// Display a single contact card on the contact list. This contact card should:
+// Be implemented by using Contact and ContactView together.
+// Show the name only, no other contact details.
+
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
-  // This model should have the attributes for
-  // a single contact: name, phone number, and email.
+  defaults: {
+    name: "Bob",
+    email: "bob@bob.com",
+    phone: "555-555-555"
+  }
 });
 
 export default Contact;
